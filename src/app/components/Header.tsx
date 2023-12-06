81b69ea752edde44088f88ae049c75c8f9475e16
AugustoMachado01 <augustomachadochieno01@gmail.com>
2023-12-06 13:09:15 +0100

    first commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <>
+      <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
+        <div className="flex items-center">
+          <Link href="/">
+            <div className="text-lg uppercase font-bold text-white">
+              Clerk App
+            </div>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+}
